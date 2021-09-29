17feb2cf54fe5205f2a072482cf1c7db7addb5e7
Justin Fleagle <19858424+SwimSwag10@users.noreply.github.com>
2021-09-29 10:03:57 -0400

    Update testingcontract.sol
    Added more content

--- testingcontract.sol
+++ testingcontract.sol
@@ -1,30 +1,40 @@
+/*
+// N O T E S
+// the front end js can only be connected to the contract once the contract is on the ethereum network.
+
+//first contract **-[-ZombieFactory-]-**
+    
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
-        string name;
-        uint dna;
+      string name;
+      uint dna;
+      uint32 level;
+      uint32 readyTime;
+      uint16 winCount;
+      uint16 lossCount;
     }
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint _dna) private {
-        //the "memory" is required for all reference types such as arrays, structs, mappings, and strings.
-        //the "public always comes after the parameters and declares that the function is a public funtion on the blockchain.
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
-        zombies.push(Zombie(_name, _dna)); 
-        //we are pushing the dna number string to the array of numbers in the struct Zombie. I believe this
-        is the make the program more readable on the backend since we are corrisponding each dna with a name.
-
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string memory _name, uint _dna) internal {
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
-        //this emits info to the event tag. This can be used on the front end of programs.
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
@@ -33,8 +43,286 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
 }
+
+// second contract **-[-ZombieFeeding-]-**
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefactory.sol";
+
+//this is an imported code from cryptokitties. It is an "interface" contract that has an empty
+//function that only returns 10 other values.
+
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract ZombieFeeding is ZombieFactory {
+
+  KittyInterface kittyContract;
+
+  modifier onlyOwnerOf(uint _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    _;
+  }
+
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal onlyOwnerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie);
+  }
+
+  // define function here
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    //kittyDna will have the same value as the "genes" uint in the getKitty function.
+    //it will also be passed in the way that the other commas equal the other values in
+    //the function so that the 10th value being genes would have 9 other commas before it.
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+}
+
+
+//third contract **-[-ZombieOwnership-]-**
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+  // 1. Define mapping here
+
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    return zombieToOwner[_tokenId];
+  }
+
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    // 2. Add the require statement here
+    // 3. Call _transfer
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable {
+
+  }
+
+}
+
+//fourth contract **-[-Ownable-]-**
+
+pragma solidity >=0.5.0 <0.6.0;
+
+* @title Ownable
+* @dev The Ownable contract has an owner address, and provides basic authorization control
+* functions, this simplifies the implementation of "user permissions".
+
+contract Ownable {
+  address private _owner;
+
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+  * account.
+ 
+  constructor() internal {
+    _owner = msg.sender;
+    emit OwnershipTransferred(address(0), _owner);
+  }
+
+  * @return the address of the owner.
+ 
+  function owner() public view returns(address) {
+    return _owner;
+  }
+
+  * @dev Throws if called by any account other than the owner.
+ 
+  modifier onlyOwner() {
+    require(isOwner());
+    _;
+  }
+
+  * @return true if `msg.sender` is the owner of the contract.
+ 
+  function isOwner() public view returns(bool) {
+    return msg.sender == _owner;
+  }
+
+  * @dev Allows the current owner to relinquish control of the contract.
+  * @notice Renouncing to ownership will leave the contract without an owner.
+  * It will not be possible to call the functions with the `onlyOwner`
+  * modifier anymore.
+ 
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipTransferred(_owner, address(0));
+    _owner = address(0);
+  }
+
+  * @dev Allows the current owner to transfer control of the contract to a newOwner.
+  * @param newOwner The address to transfer ownership to.
+ 
+  function transferOwnership(address newOwner) public onlyOwner {
+    _transferOwnership(newOwner);
+  }
+
+  * @dev Transfers control of the contract to a newOwner.
+  * @param newOwner The address to transfer ownership to.
+ 
+  function _transferOwnership(address newOwner) internal {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(_owner, newOwner);
+    _owner = newOwner;
+  }
+}
+
+//fifth contract **-[-ZombieHelper-]-**
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+  uint levelUpFee = 0.001 ether;
+
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    _;
+  }
+
+  function withdraw() external onlyOwner {
+    address _owner = owner();
+    _owner.transfer(address(this).balance);
+  }
+
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
+  }
+
+  function changeName(uint _zombieId, string calldaata _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) onlyOwnerOf(_zombieId) {
+    zombies[_zombieId].dna = _newDna;
+  }
+
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
+  }
+
+}
+
+//sixth contract **-[-ZombieAttack-]--**
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+  uint randNonce = 0;
+  uint attackVictoryProbability = 70;
+
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+  }
+
+  function attack(uint _zombieId, uint _targetId) external onlyOwnerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    } else {
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+      _triggerCooldown(myZombie);
+    }
+  }
+}
+
+//seventh contract **-[-ERC721-]-**
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ERC721 {
+  event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+  event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+
+  function balanceOf(address _owner) external view returns (uint256);
+  function ownerOf(uint256 _tokenId) external view returns (address);
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+  function approve(address _approved, uint256 _tokenId) external payable;
+}
+
+
+*/
